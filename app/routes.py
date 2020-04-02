4639c8d406db579838c5e2c17a11717f487793ea
laviniaclare <laviniaclare@gmail.com>
2020-04-02 13:44:14 -0700

    Set up basic db connection

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(user='postgres',pw='engineTest888',host='localhost', port='5555',db='zesty')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
