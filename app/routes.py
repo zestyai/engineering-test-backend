dda15bb8b469ac1bdade694b3b858a18ceebc186
laviniaclare <laviniaclare@gmail.com>
2020-04-03 15:42:32 -0700

    Implement display_image route

--- app/routes.py
+++ app/routes.py
@@ -1,55 +1,56 @@
-from shared_resources import app
 from models import Property
+from shared_resources import app
 
+from flask import send_file
+import io
+import requests
 
-@app.route('/')
-def landing():
-	return ""
 
 @app.route('/display/<string:property_id>', methods=['GET'])
 def display(property_id):
-	"""
-	Display an image by property ID.
-	"""
-	# Part 1: Given a propertyId as input, find the image URL from the database,
-	# download it from Google Cloud Storage and return the binary image
-	# as output in a format suitable for browser display.
+    """
+    Display an image by property ID.
 
-	# Part 2: Add an option to also overlay the parcel and/or buildings
-	# (parcel_geo and buildings_geo fields in the database) on the image.
-	# You can optionally add parameters for the color of each overlay, or use a default for each.
+    Args: property_id (string)
 
-	return str(property_id)
+    Returns: jpeg file
+    """
+    prop = Property.query.get(property_id)
+    image_url = prop.image_url
+    url_resp = requests.get(image_url, stream=True)
+
+    return send_file(
+        io.BytesIO(url_resp.content),
+        mimetype='image/jpeg',
+        as_attachment=True,
+        attachment_filename='%s.jpg' % property_id)
 
 @app.route('/find', methods=['GET'])
 def find():
-	"""
-	Search properties within a geographical area.
-	"""
-	# Take a GeoJSON object geoJson and a search radius distance (in meters) as inputs.
-	# Return all property IDs that are within distance meters of geoJson.
-	# Use the geocode_geo field of the property table for your query.
-	pass
+    """
+    Search properties within a geographical area.
+    """
+    return ''
 
 @app.route('/statistics/<string:property_id>/<int:distance>', methods=['GET'])
 def statistics(property_id, distance):
-	"""
-	Calculate geographic data about all properties within a given distance from a reference property.
+    """
+    Calculate geographic data about all properties within a given distance
+    (in meters) from a reference property.
 
-	Return the following statistics:
-		- parcel area (meters squared)
-		- buildings areas (array, meters squared)
-		- buildings distances to center (array, meters squared).
-		  Distance to center is the distance from the building to
-		  the geocode_geo field in the property table
-		- zone density (percentage).
-		  Create a "zone" geography, which is a buffer of distance meters
-		  around the geocode_geo point. Then, calculate the percentage of
-		  that zone geography which has buildings in it.
-	"""
-	# take propertyId and distance (in meters) as inputs.
-	pass
+    Returns the following statistics:
+        - parcel area (meters squared)
+        - buildings areas (array, meters squared)
+        - buildings distances to center (array, meters squared).
+          Distance to center is the distance from the building to
+          the geocode_geo field in the property table
+        - zone density (percentage).
+          Create a "zone" geography, which is a buffer of distance meters
+          around the geocode_geo point. Then, calculate the percentage of
+          that zone geography which has buildings in it.
+    """
+    return ''
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
