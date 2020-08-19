9501c3f1ce77bd600670e15d86e936ffc9c8d216
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2020-08-19 13:50:47 -0400

    set up local db

---
+++ backend/src/database.ts
@@ -0,0 +1,24 @@
+import { Client } from 'pg';
+import env from 'env-var';
+
+const databaseUrl: string = env.get('DATABASE_URL').required().asUrlString();
+let dbClient: Client;
+
+export async function initDbConnection() {
+  async function testConnection(resolve: any, delaySeconds = 2) {
+    try {
+      dbClient = new Client(databaseUrl);
+      await dbClient.connect();
+      console.info('Connected to PostgreSQL database');
+      resolve();
+    } catch (err) {
+      console.error(err.message);
+      console.info(`Waiting for database... Retrying in ${delaySeconds} seconds.`);
+      setTimeout(async () => testConnection(resolve, delaySeconds * 2), delaySeconds * 1000);
+    }
+  }
+
+  return new Promise(async (resolve) => {
+    await testConnection(resolve);
+  });
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
 import { Express } from 'express';
+import { initDbConnection } from './database';
 
 export async function initServer(app: Express, port: number): Promise<void> {
+  try {
+    await initDbConnection();
+  } catch (error) {
+    console.error(`Failed to connect to PostgreSQL database: ${error.message}`);
+  }
+
   try {
     app.listen(port, () => {
       console.info(`Spicy API listening at http://localhost:${port}`);
     });
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 }
